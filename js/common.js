03da713797248a3bc3832192b6788c86103971ed
vmonasteretskyy <v.monasteretskyy@gmail.com>
2022-05-16 17:41:18 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
-
+    // trusted us - slider
+    $('.trusted-slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
 });
