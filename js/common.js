4f9c59e08298e1b2607c05ca485a4849d512abc0
vmonasteretskyy <v.monasteretskyy@gmail.com>
2022-05-17 14:27:09 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -8,6 +8,52 @@ $(document).ready(function () {
         arrows: false,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
     });
+
+    // header - add shadow
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 40) {
+            $('header.header').addClass('header__shadow');
+        } else {
+            $('header.header').removeClass('header__shadow')
+        }
+    });
+
+    // menu mobile
+    $('.submenu').click(function () {
+        $(this).find('ul').slideToggle();
+    });
+
+    $('.burger').click(function () {
+        $(this).toggleClass('open');
+        $('.mobilemenu-wrapper').toggleClass('open');
+    });
+
 
 });
